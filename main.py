f3a9ef5a24864aff3d1fb68e51c7868613576c8e
Illya Antonyuk <illantalex84@gmail.com>
2024-10-23 21:45:00 +0200

    fixed timestamp send

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ from control import at_mqtt
 from control import bme280_ctl
 from control import i2c_power_controller
 
-picam2 = Picamera2()
-
 dotenv.load_dotenv()
 # MQTT settings
 CLIENT_ID = os.getenv('CLIENT_ID')
@@ -43,8 +41,6 @@ last_received_time = None
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-    client.subscribe(RTC_TOPIC)
-    client.subscribe(TIME_INTERVAL_TOPIC)
 
 
 def on_message(client, userdata, msg):
@@ -78,6 +74,22 @@ if __name__ == '__main__':
             all_exit()
         time.sleep(3)
 
+    curr_time = at_mqtt.read_clock()
+    print(f"Current time: {curr_time}")
+    with open("/home/pi/rtc.txt", "r") as f:
+        last_received_time = f.read().strip()
+    with open("/home/pi/time_interval.txt", "r") as f:
+        time_interval = f.read().strip()
+
+    last_sent_time = datetime.strptime(last_received_time, DATETIME_FORMAT)
+    current_time = datetime.strptime(curr_time, DATETIME_FORMAT)
+    time_diff = current_time - last_sent_time
+
+    time_remaining = int(time_interval) * 3600 - time_diff.total_seconds()
+    if time_remaining > 20:
+        i2c_power_controller.set_time_command(int(time_remaining) * 1000000)
+        all_exit()
+
     time.sleep(10)
 
     # Read current time
@@ -94,32 +106,25 @@ if __name__ == '__main__':
     mqttc.unsubscribe(RTC_TOPIC)
     mqttc.unsubscribe(TIME_INTERVAL_TOPIC)
 
-    if last_received_time is None or time_interval is None:
+    if last_received_time is None:
         print("No RTC or time interval received")
         with open("/home/pi/rtc.txt", "r") as f:
             last_received_time = f.read().strip()
+    if time_interval is None:
         with open("/home/pi/time_interval.txt", "r") as f:
             time_interval = f.read().strip()
 
-    mqttc.publish(RTC_TOPIC, last_received_time, qos=1, retain=True)
+    mqttc.publish(RTC_TOPIC, curr_time, qos=1, retain=True)
     mqttc.publish(TIME_INTERVAL_TOPIC, str(time_interval), qos=1, retain=True)
 
     with open("/home/pi/rtc.txt", "w") as f:
-        f.write(last_received_time)
+        f.write(curr_time)
     with open("/home/pi/time_interval.txt", "w") as f:
         f.write(time_interval)
 
-    last_sent_time = datetime.strptime(last_received_time, DATETIME_FORMAT)
-    current_time = datetime.strptime(curr_time, DATETIME_FORMAT)
-    time_diff = current_time - last_sent_time
-
-    time_remaining = int(time_interval) * 3600 - time_diff.total_seconds()
-    if time_remaining > 60:
-        i2c_power_controller.set_time_command(int(time_remaining) * 1000000)
-        all_exit()
-
     i2c_power_controller.set_time_command(int(time_interval) * 3600 * 1000000)
 
+    picam2 = Picamera2()
 
     preview_config = picam2.create_preview_configuration(main={"size": (1280, 960)})
     picam2.configure(preview_config)
@@ -139,11 +144,11 @@ if __name__ == '__main__':
     # time.sleep(3)
 
     mqttc.publish(RTC_TOPIC, curr_time, qos=1, retain=True)
-    mqttc.publish(TEMPERATURE_TOPIC, str(bme280_ctl.get_temperature()), qos=1)
-    mqttc.publish(HUMIDITY_TOPIC, str(bme280_ctl.get_humidity()), qos=1)
-    mqttc.publish(PRESSURE_TOPIC, str(bme280_ctl.get_pressure()), qos=1)
+    mqttc.publish(TEMPERATURE_TOPIC, str(bme280_ctl.get_temperature()), qos=1, retain=True)
+    mqttc.publish(HUMIDITY_TOPIC, str(bme280_ctl.get_humidity()), qos=1, retain=True)
+    mqttc.publish(PRESSURE_TOPIC, str(bme280_ctl.get_pressure()), qos=1, retain=True)
     # mqttc.publish(TEST_DATA_TOPIC, "Hello World", qos=1, retain=True)
-    mqttc.publish(YOLO_DATA_TOPIC, yolo_output, qos=1)
+    mqttc.publish(YOLO_DATA_TOPIC, yolo_output, qos=1, retain=True)
 
     time.sleep(5)
     mqttc.loop_stop()
